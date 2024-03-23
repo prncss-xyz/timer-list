a31cbcb2adb22f0428bb9edaae814d2eb625b2eb
Juliette Lamarche <princesse@princesse.xyz>
2024-03-23 15:17:51 -0400

    feat(timeInput): debounce

--- src/components/timeInput.tsx
+++ src/components/timeInput.tsx
@@ -1,26 +1,32 @@
 import { useAtom } from "jotai";
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { TextInput } from "react-native";
 
 import { getIdItemSecondsAtom } from "../list";
 import { styles } from "../styles";
+import { getDebouncer } from "../utils/debouncer";
 import { fromSeconds, toSeconds } from "../utils/numbers";
 
 export function TimeInput({ id, color }: { id: string; color?: string }) {
   const [seconds, setSeconds] = useAtom(
     useMemo(() => getIdItemSecondsAtom(id), [id]),
   );
+  const debouncedSetseconds = useCallback(getDebouncer(setSeconds, 200), [
+    setSeconds,
+  ]);
+  const [v, setV] = useState(seconds);
   const onTextInput = useCallback(
     (value: string) => {
       const seconds_ = toSeconds(value);
-      setSeconds(seconds_);
+      setV(seconds_);
+      debouncedSetseconds(seconds_);
     },
-    [setSeconds],
+    [setV, debouncedSetseconds],
   );
   return (
     <TextInput
       style={[{ backgroundColor: color }, styles.timeInput]}
-      value={fromSeconds(seconds ?? 0)}
+      value={fromSeconds(v ?? 0)}
       onChangeText={onTextInput}
       inputMode="decimal"
     />
