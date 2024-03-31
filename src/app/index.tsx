17755c14a53f006935789582ac06e8eaf53818dc
Juliette Lamarche <princesse@princesse.xyz>
2024-03-30 21:06:41 -0400

    Fix(expo-router): blank page issue

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,5 @@
-import { useRouter, useFocusEffect } from "expo-router";
 import { Text } from "react-native";
 
 export default function Page() {
-  const router = useRouter();
-  useFocusEffect(() => {
-    router.replace("/list");
-  });
-
-  return <Text>redirectiong...</Text>;
+  return <Text>Hello!</Text>;
 }
